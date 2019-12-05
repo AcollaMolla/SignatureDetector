a01d896d8738ad1b345f36b42a42cce98a3f1234
Anton Engman <anton.engman@knowit.se>
2019-12-05 14:35:31 +0100

    Created project

---
+++ SignatureDetector/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SignatureDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
